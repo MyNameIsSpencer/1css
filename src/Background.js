54935dc15ef0ca405ba20ef7464fe0316cf36eaf
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-02-23 17:18:06 -0500

    add background class

--- src/Background.js
+++ src/Background.js
@@ -12,6 +12,9 @@ class Background extends Component {
   render() {
     return (
       <div>
+        <h2 className='page-title'>
+          Backgrounds
+        </h2>
         <div className="arranger">
           <div className="back1">
             <h4 className="blue-text">
@@ -34,24 +37,112 @@ class Background extends Component {
             </h4>
           </div>
         </div>
+
         <div className="arranger">
-          <div className="back5">
+          <div className="repeater1">
             <h4 className="labeller">
               Line it up!
             </h4>
           </div>
-          <div className="back6">
-          
+          <div className="repeater2">
           </div>
-          <div className="back7">
+          <div className="repeater3">
+            <h4 className="labeller">
+              Small Default
+            </h4>
+          </div>
+          <div className="repeater4">
+            <h4 className="labeller">
+              Round
+            </h4>
+          </div>
+        </div>
+
+        <div className="arranger">
+          <div className="repeater5">
+            <h4 className="labeller">
+              Repeat X
+            </h4>
+          </div>
+          <div className="repeater6">
+            <h4 className="labeller">
+              Repeat Y
+            </h4>
+          </div>
+          <div className="repeater7">
+            <h4 className="labeller">
+              Space
+            </h4>
+          </div>
+          <div className="repeater8">
+            <h4 className="labeller">
+              No Repeat Centre
+            </h4>
+          </div>
+        </div>
+
+        <div className="arranger">
+          <div className="positioner1">
+            <h4 className="labeller">
+              Left
+            </h4>
+          </div>
+          <div className="positioner2">
+            <h4 className="labeller">
+              Top Right
+            </h4>
+          </div>
+          <div className="positioner3">
+            <h4 className="labeller">
+              70% 33%
+            </h4>
+          </div>
+          <div className="positioner4">
+            <h4 className="labeller">
+              15px 250px
+            </h4>
+          </div>
+        </div>
+
+{/*
+        <div className="arranger">
+          <div className="back13">
             <h4 className="labeller">
             </h4>
           </div>
-          <div className="back8">
+          <div className="back14">
+            <h4 className="labeller">
+            </h4>
+          </div>
+          <div className="back15">
+            <h4 className="labeller">
+            </h4>
+          </div>
+          <div className="back16">
             <h4 className="labeller">
             </h4>
           </div>
         </div>
+
+        <div className="arranger">
+          <div className="back13">
+            <h4 className="labeller">
+            </h4>
+          </div>
+          <div className="back14">
+            <h4 className="labeller">
+            </h4>
+          </div>
+          <div className="back15">
+            <h4 className="labeller">
+            </h4>
+          </div>
+          <div className="back16">
+            <h4 className="labeller">
+            </h4>
+          </div>
+        </div>
+*/}
       </div>
     )
   }
